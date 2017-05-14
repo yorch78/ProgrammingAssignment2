dbf62d9dbdfd03259d345e63b820e36d6edcf07b
Jorge Robledo <yorch78@hotmail.es>
2017-05-14 13:35:12 +0200

    Tested and commented solution
    Functions tested and code commented.

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,98 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This piece of code are two functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix: creates a special object "matrix" class that cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+## Input parameter: x (by default creates an empty matrix object)
+	
+		## Inverse property initialization
         i <- NULL
+        
+        ## Setter method
         set <- function(y) {
-                x <<- y
-                i <<- NULL
+        		## Assigns object y to the special matrix x
+        		x <<- y
+        		i <<- NULL
         }
-        get <- function() x
-        setInverse <- function(inverse) i <<- inverse
-        getInverse <- function() i
-        list(set = set, get = get,
-             setInverse = setInverse,
-             getInverse = getInverse)
+        
+        ## Getter method
+        get <- function() {
+        		## Returns the special matrix
+        		x	
+        } 
+        
+        ## Setter method
+        setInverse <- function(inverse) {
+        		## Assigns object inverse containing the inverse matrix to i
+        		i <<- inverse
+        }
+        
+        ## Getter method
+        getInverse <- function() {
+        		## Returns the inverse of the special matrix
+        		i	
+        } 
+        
+        ## Returns a list with the special matrix available methods
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: calculates the inverse of the special "matrix" returned by makeCacheMatrix,
+## just only when the matrix changes. If it's not changed, this funtion must return the
+## inverse cached.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Returns a matrix that is the inverse of 'x'
         i <- x$getInverse()
+        
+        ## If the inverse is set, return the cached inverse and finish
         if(!is.null(i)) {
-                message("getting cached data")
-                return(i)
+        		message("getting cached data")
+            return(i)
         }
+        
+        ## Get the original matrix to proceeed with the inverse calculation
         data <- x$get()
+        
+        ## Calculates the inverse matrix of data matrix
         i <- solve(data)
+        
+        ## Set the new inverse to the object
         x$setInverse(i)
+        
+        ## Returns the inverse calculated
         i
 }
+
+
+## Test cases execution
+> miMatriz <-matrix(c(0,1,0,1,0,0,1,0,1),3,3)
+> miMatriz
+     [,1] [,2] [,3]
+[1,]    0    1    1
+[2,]    1    0    0
+[3,]    0    0    1
+> miM <-makeCacheMatrix(miMatriz)
+> miM$get()
+     [,1] [,2] [,3]
+[1,]    0    1    1
+[2,]    1    0    0
+[3,]    0    0    1
+> cacheSolve(miM)
+     [,1] [,2] [,3]
+[1,]    0    1    0
+[2,]    1    0   -1
+[3,]    0    0    1
+> cacheSolve(miM)
+getting cached data
+     [,1] [,2] [,3]
+[1,]    0    1    0
+[2,]    1    0   -1
+[3,]    0    0    1
+> miM$get()%*%cacheSolve(miM)
+getting cached data
+     [,1] [,2] [,3]
+[1,]    1    0    0
+[2,]    0    1    0
+[3,]    0    0    1
